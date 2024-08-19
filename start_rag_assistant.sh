c512afc0c670b22c8c4059da0196ba5e0e868aac
Dennis Herbrik <dennisherbrik1988@gmail.com>
2024-08-19 19:00:30 +0200

    Updated bash files

--- start_rag_assistant.sh
+++ start_rag_assistant.sh
@@ -2,11 +2,16 @@
 
 echo "Start the rag-assistant-api"
 cd ../rag_assistant_api
-poetry run python -m src.rag_assistant_api.main &
+source ~/.virtualenvs/rag_assistant_env/bin/activate
+python -m src.rag_assistant_api.main &
 
+deactivate
 echo "Start the rag-chat-ui"
 cd ../rag_chat_ui
-poetry run python -m src.chat_ui.main &
+source ~/.virtualenvs/rag_chat_ui_env/bin/activate
+python -m src.chat_ui.main &
 
-
-
+echo "Start speech to text"
+cd ../speech_2_text
+source ~/.virtualenvs/jarvis_env/bin/activate
+python src/speech_2_text/main.py
